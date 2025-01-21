42e340e4cc596c51f37cd6d862aec3a2e9141043
Hénoc <amaviganhenoc@gmail.com>
2025-01-21 22:43:35 +0000

    add bottom nav bar

--- app/src/main/java/tg/ulcrsandroid/carpooling/presentation/activities/HomeActivity.kt
+++ app/src/main/java/tg/ulcrsandroid/carpooling/presentation/activities/HomeActivity.kt
@@ -2,12 +2,17 @@ package tg.ulcrsandroid.carpooling.presentation.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import tg.ulcrsandroid.carpooling.R
 
 class HomeActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -21,6 +26,14 @@ class HomeActivity : AppCompatActivity(), OnMapReadyCallback {
         // Initialiser la carte
         val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        // Initialiser le BottomNavigationView et le NavController
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+            .setupWithNavController(navController)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
