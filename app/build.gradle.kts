bcae16b3c424bd13740c2a4ff292cfbd8eb8ccff
henoc97 <amaviganhenoc@gmail.com>
2025-02-01 14:44:21 +0000

    merge all branchs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation(libs.androidx.activity)
     implementation (libs.androidx.navigation.fragment.ktx)
     implementation (libs.androidx.navigation.ui.ktx)
